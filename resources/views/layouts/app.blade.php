5967dd652e8d1ef370988cb759f567e70b4d2fab
Clarence Bakirtzidis <clarenceb@gmail.com>
2019-03-27 10:14:36 +0800

    Bumbp to V2 for test purposes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,7 +32,7 @@
 
                 <!-- Branding Image -->
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    Task List
+                    Task List v2
                 </a>
             </div>
 
